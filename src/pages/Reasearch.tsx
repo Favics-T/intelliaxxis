74a2215ac6dd7f6ed2ae650ea2e55c1884c40778
unknown <taiwokolapo533@gmail.com>
2026-03-18 12:03:27 +0100

    added reasearch page

---
+++ src/pages/Reasearch.tsx
@@ -0,0 +1,7 @@
+import type { FC } from 'react';
+
+export const ResearchPage: FC = () => (
+  <div className="animate-[fade-up_0.5s_ease-out_both]">
+    <p className="text-textSecondary">Research chat coming soon...</p>
+  </div>
+);
