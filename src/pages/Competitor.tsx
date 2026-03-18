42244c1800e56996b39ac9de645799ddab50dfb8
unknown <taiwokolapo533@gmail.com>
2026-03-18 12:04:27 +0100

    added competitor page

---
+++ src/pages/Competitor.tsx
@@ -0,0 +1,7 @@
+import type { FC } from 'react';
+
+export const CompetitorsPage: FC = () => (
+  <div className="animate-[fade-up_0.5s_ease-out_both]">
+    <p className="text-textSecondary">Competitor tracker coming soon...</p>
+  </div>
+);
