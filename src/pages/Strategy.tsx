d1a1de7a5863ada2098fc7e5f5a15ff6bcf33716
unknown <taiwokolapo533@gmail.com>
2026-03-18 12:02:13 +0100

    added Strategy page

---
+++ src/pages/Strategy.tsx
@@ -0,0 +1,7 @@
+import type { FC } from 'react';
+
+export const StrategyPage: FC = () => (
+  <div className="animate-[fade-up_0.5s_ease-out_both]">
+    <p className="text-textSecondary">Strategy center coming soon...</p>
+  </div>
+);
