93344e624c8cde596574e7d61e11ce6893693fc7
unknown <taiwokolapo533@gmail.com>
2026-03-18 12:03:55 +0100

    added digest page

---
+++ src/pages/Digest.tsx
@@ -0,0 +1,7 @@
+import type { FC } from 'react';
+
+export const DigestPage: FC = () => (
+  <div className="animate-[fade-up_0.5s_ease-out_both]">
+    <p className="text-textSecondary">Weekly digest coming soon...</p>
+  </div>
+);
